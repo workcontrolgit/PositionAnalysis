b0e6440076d00c0022a1051b3a10728651f1f1de
Fuji Nguyen <nguyenfd@state.gov>
2026-08-10 12:09:42 -0400

    folder clean up

--- src/OracleSqlclAgent/AgentClassSkill.cs
+++
@@ -1,9 +0,0 @@
-namespace OracleSqlclAgent;
-
-public abstract class AgentClassSkill
-{
-    public abstract string Name { get; }
-    public abstract string Description { get; }
-    protected abstract string Instructions { get; }
-    public string GetInstructions() => Instructions;
-}
