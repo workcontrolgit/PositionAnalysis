4a4032184987c3d44f43332609de4e6c30014bbe
Fuji Nguyen <nguyenfd@state.gov>
2026-08-13 00:04:56 -0400

    Remove old SchedulePC/schedulepc folders after migration to PositionAnalysis

--- src/SchedulePCMcp/Domain/Entities/QueueStatus.cs
+++
@@ -1,6 +0,0 @@
-namespace SchedulePCMcp.Domain.Entities;
-
-public sealed record QueueStatus(int Pending, int InProgress, int Complete, int Failed)
-{
-    public bool IsDrained => Pending == 0 && InProgress == 0;
-}
--- src/schedulepc/ISchedulePcMcpClient.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json;
-
-namespace SchedulePC;
-
-public interface ISchedulePcMcpClient
-{
-    Task<JsonElement> CallToolAsync(string name, object arguments);
-}
